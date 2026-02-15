d80b443b4583948f8a70131f5380e856bcc6f714
Erchask <chadi.kini07@gmail.com>
2026-02-15 20:35:24 +0100

    Create Stile.java

---
+++ src/main/java/org/example/Stile.java
@@ -0,0 +1,15 @@
+public class Stile {
+    public static final String RESET = "\u001B[0m";
+    public static final String ROSSO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String BIANCO = "\u001B[37m";
+    public static final String BLU = "\u001B[34m";
+
+    public static final String SfondoG = "\u001B[43m";
+    public static final String SfondoR = "\u001B[41m";
+    public static final String SfondoV = "\u001B[42m";
+    public static final String SfondoB = "\u001B[44m";
+
+    public static final String GRASSETTO = "\u001B[1m";
+    public static final String SOTTOLINEATO = "\u001B[4m";
+}
