cd44b6fa3a1f16a823eb60fde263d35d9fea63df
Erchask <chadi.kini07@gmail.com>
2026-02-15 20:33:56 +0100

    Update MainClient.java

--- src/main/java/org/example/MainClient.java
+++ src/main/java/org/example/MainClient.java
@@ -4,5 +4,9 @@ public class MainClient {
     public static void main(String[] args) {
         Client client= new Client("Chadi");
         client.connetti("localHost",4000);
+	System.out.println(Stile.BIANCO + Stile.SfondoV + "Client connesso al server" + Stile.RESET);
+	client.scrivi();
+	client.leggi();
+	
     }
     }
